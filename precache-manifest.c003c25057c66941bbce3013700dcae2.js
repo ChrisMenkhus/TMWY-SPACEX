164ec1040b5b5f3b32d2664e645c17e761732050
Chris Menkhus <menkhus.chris@gmail.com>
2020-06-14 01:33:32 -0600

    Updates

---
+++ precache-manifest.c003c25057c66941bbce3013700dcae2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6faf153f876cc90727b6a3913a8ac9aa",
+    "url": "/takemewithyou-spacex/index.html"
+  },
+  {
+    "revision": "f703aaec3bad21a2d9c1",
+    "url": "/takemewithyou-spacex/static/css/main.13f9e700.chunk.css"
+  },
+  {
+    "revision": "c778e6f8bcfa4285f927",
+    "url": "/takemewithyou-spacex/static/js/2.1a8cd3b3.chunk.js"
+  },
+  {
+    "revision": "6ef718eeb39c03c0fcdab1a60b0c6b00",
+    "url": "/takemewithyou-spacex/static/js/2.1a8cd3b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f703aaec3bad21a2d9c1",
+    "url": "/takemewithyou-spacex/static/js/main.f3f17146.chunk.js"
+  },
+  {
+    "revision": "f2cb4e47a1864be13ff5",
+    "url": "/takemewithyou-spacex/static/js/runtime-main.2c550c27.js"
+  },
+  {
+    "revision": "5f515653cc578240bddcf843e00490fe",
+    "url": "/takemewithyou-spacex/static/media/dark.5f515653.png"
+  },
+  {
+    "revision": "fa285ec59f6aeb25f7c3a3e831efdd07",
+    "url": "/takemewithyou-spacex/static/media/dark2.fa285ec5.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/takemewithyou-spacex/static/media/logo.5d5d9eef.svg"
+  }
+]);
